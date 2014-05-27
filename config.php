2ca385a02e732a1952de6c40bac668a769be94a7
Bardi Harborow <bardi_harborow@yahoo.com.au>
2014-05-27 19:05:07 -0400

    Add deposit config.

--- config.php
+++ config.php
@@ -7,4 +7,8 @@ define('MYSQL_WRITE_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('MYSQL_WRITE_PASS'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('MYSQL_READ_USER', 'netvend_readonly');
 define('MYSQL_READ_PASS', 'CHANGE_THIS')
+
+/* Deposit Details */
+define('DEPOSIT_ADDRESS', 'CHANGE_THIS');
+define('DEPOSIT_CONFIRMATIONS', 3);
 ?>
